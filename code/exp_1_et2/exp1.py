b51d9ac42a83358795d36a4489d40b79628ea565
jaklecrack <musse.marina@orange.fr>
2024-05-16 23:06:14 +0200

    extraction de l'attention relative entre deux mots donnés par leur position dans le corpus

--- code/exp_1_et2/exp1.py
+++ code/exp_1_et2/exp1.py
@@ -43,7 +43,13 @@ model=CamembertModel.from_pretrained(modelName, output_attentions=True)
 def tokenize(sentences,tokenizer):
     """ returns the tokens of the sentences passed sentences as computed by the model """
     tokens=tokenizer(sentences,return_tensors='pt',padding=True)
-    return tokens['input_ids']
+    return tokens['input_ids'] # torch.tensor
+
+def untokenize(input_ids,tokenizer):
+    output=[]
+    for input in input_ids:
+        output.append(tokenizer.convert_ids_to_tokens(input))
+    return output
 
 
 def attentionMatrices(sentences,tokenizer,model):
@@ -64,14 +70,46 @@ tokens = tokenize(corpus, tokenizer)
 layer0 = LayerAttentionMatrices(corpus,tokenizer,model,0)
 attention_matrices = attentionMatrices(corpus,tokenizer,model)
 
-
-print(len(tokens))
+print(untokenize(tokens,tokenizer))
+print("nb of tokens:", len(tokens[0]))
 print(layer0.shape)
 print(len(attention_matrices))
-print(attention_matrices.head())
+print(attention_matrices[0].shape)
+
+
+### CALCUL DE LA MOYENNE DE L'ATTENTION ###########################
 
 
 
+### RECUPERATION DE L'ATTENTION RELATIVE ENTRE DEUX MOTS DONNÉS ###
+
+
+def LayerRelativeAttention(pos1, pos2, layer_attention_matrix):
+    nb_heads = attention_matrices[0].shape[1]
+    layer_relative_attention = []
+    for head in range(nb_heads):
+        layer_relative_attention.append(layer_attention_matrix[0][head][pos1][pos2])
+    return [tensor.item() for tensor in layer_relative_attention]
+
+def relativeAttention(pos1,pos2,attention_matrices):
+    nb_layers = len(attention_matrices)
+    relative_attention = []
+    for layer in range(nb_layers):
+        relative_attention.append(LayerRelativeAttention(pos1,pos2,attention_matrices[layer]))
+    return relative_attention
+
+
+relatt1 = LayerRelativeAttention(0,1,attention_matrices[0])
+relatt2 = LayerRelativeAttention(0,1,attention_matrices[0])
+
+resultat = [[relatt1[i], relatt2[i]] for i in range(min(len(relatt1), len(relatt2)))]
+print(resultat)
+
+
+test = relativeAttention(0,1,attention_matrices)
+print(test)
+print(len(test))
+print(len(test[0]))
 
 
 
