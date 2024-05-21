ec4baf5658d821267fa45a8151915e02cb7d98aa
jaklecrack <musse.marina@orange.fr>
2024-05-21 22:54:44 +0200

    cacul tokens, embeddings et similarité cosine exp2

--- code/exp_1_et2/exp2.py
+++ code/exp_1_et2/exp2.py
@@ -1,5 +1,6 @@
 from lxml import etree
 import os
+import torch
 
 
 
@@ -32,4 +33,60 @@ for citation in citations:
     print("----")
 
 
+### TOKENISATION ET DETOKENISATION #####################################################################
 
+
+
+def tokenize(sentences,tokenizer):
+    """ returns the tokens of the sentences passed sentences as computed by the model """
+    tokens=tokenizer(sentences,return_tensors='pt',padding=True)
+    return tokens['input_ids']
+
+def untokenize(input_ids,tokenizer):
+    output=[]
+    for input in input_ids:
+        output.append(tokenizer.convert_ids_to_tokens(input))
+    return output
+
+
+### CALCUL DES EMBEDDINGS ###########################################################################
+
+
+def computeEmbeddings(tokens,model): 
+    """ returns input embedding of arguments tokens"""
+    
+    output_embeddings = []
+   
+    ####### INPUT EMBEDDINGS ###### 
+    embedding_layer = model.embeddings
+    output_embeddings.append(embedding_layer(tokens))
+
+    for i in range(11):
+        encoder_layer = model.encoder.layer[i]
+        encoder_embeddings = encoder_layer(hidden_states=output_embeddings[i])[0]
+        output_embeddings.append(encoder_embeddings)
+
+    final_embeddings = model.encoder(output_embeddings[0])[0]
+    output_embeddings.append(final_embeddings)
+    return output_embeddings
+
+
+
+### SIMILARITE COSINUS ####################################################################
+
+
+def cosineSimilarity(embeddings1,embeddings2):
+    CosSim = torch.nn.CosineSimilarity(dim=0, eps=1e-6)
+    output=CosSim(embeddings1,embeddings2)
+    return(output.item())
+
+
+### MAIN ##################################################################################
+
+
+def main():
+    print("hello")
+
+
+if __name__ == "__main__":
+    main()
